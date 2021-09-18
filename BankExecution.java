7234c13f85f9ed7ec17617845a9342488c858b3b
DeepikaaS <90097534+DeepikaaS@users.noreply.github.com>
2021-09-18 21:43:36 +0530

    Add files via upload

---
+++ BankExecution.java
@@ -0,0 +1,17 @@
+package week3.day1;
+public class BankExecution  {
+public void deposit() {
+	// TODO Auto-generated method stub
+	System.out.println("This is main Deposit");
+
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		AxisBank obj=new AxisBank();
+		obj.deposit();
+		BankInfo obj1=new BankInfo();
+		obj1.deposit();
+
+	}
+
+}
