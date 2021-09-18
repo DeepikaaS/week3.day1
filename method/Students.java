25abc17f50c91b3df087497cade24638b0afbb08
DeepikaaS <90097534+DeepikaaS@users.noreply.github.com>
2021-09-18 21:40:49 +0530

    Add files via upload

---
+++ method/Students.java
@@ -0,0 +1,26 @@
+package org.method;
+
+public class Students {
+	public void getStudentInfo(int id) {
+		// TODO Auto-generated method stub
+		 System.out.println("The Student id  is"+id);
+
+	}
+	public void getStudentInfo(int id,String name) {
+		// TODO Auto-generated method stub
+		 System.out.println("The Student id  is"+id);
+		 System.out.println("The Student Name  is"+name);
+
+	}
+	private void getStudentInfo(String email,long phoneNumber) {
+		// TODO Auto-generated method stub
+		 System.out.println("The Student Email id is "+email);
+		 System.out.println("The Student Name  is "+phoneNumber);
+	}
+	public static void main(String[] args) {
+		Students obj=new Students();
+		obj.getStudentInfo(5);
+		obj.getStudentInfo(1, "Deepikaa");
+		obj.getStudentInfo("deepikss.Sen@gmail.com", 5678765678L);
+	}
+}
